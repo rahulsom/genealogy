63397a14ddccfe064919ffd642b18d8073944f09
Rahul Somasunderam <rahulsom@noreply.github.com>
2025-09-21 10:16:49 -0700

    chore: Install/configure spotless
    Also ignore formatting changes in blame

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   id("com.github.rahulsom.waena.root").version("0.18.1")
   id("com.github.rahulsom.waena.published").version("0.18.1")
   id("io.freefair.lombok").version("8.14.2")
+  id("com.diffplug.spotless").version("6.25.0")
 }
 
 repositories {
@@ -11,3 +12,24 @@ repositories {
 
 group = "com.github.rahulsom"
 description = "Genealogy"
+
+spotless {
+  java {
+    importOrder()
+    removeUnusedImports()
+    googleJavaFormat()
+  }
+  kotlinGradle {
+    target("*.gradle.kts", "settings.gradle.kts")
+    ktlint()
+  }
+  format("misc") {
+    target("*.md", ".gitignore")
+    prettier(mapOf("prettier" to "2.8.8"))
+  }
+  format("yaml") {
+    target("**/*.yml")
+    prettier(mapOf("prettier" to "2.8.8"))
+      .config(mapOf("parser" to "yaml"))
+  }
+}
